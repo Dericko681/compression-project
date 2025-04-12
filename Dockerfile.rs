ff5eb4d8abc93e00c012ab5a62da460752e4495a
dericko681 <abonghoderick@gmail.com>
2025-04-12 11:31:32 +0100

    first version

--- Dockerfile.rs
+++
@@ -1,9 +0,0 @@
-FROM rust:1.70-slim AS builder
-
-WORKDIR /app
-
-# Copy full source
-COPY rust-compressor .
-
-# Build the library
-RUN cargo build --release --lib
--- rust-compressor/src/algos/mod.rs
+++
@@ -1,2 +0,0 @@
-pub mod lz;
-pub mod le;
