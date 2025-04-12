72762c3a57120cefaa24ed9262b232c522f937b1
dericko681 <abonghoderick@gmail.com>
2025-04-12 07:40:59 +0100

    Dockerfile

--- Dockerfile.rs
+++ Dockerfile.rs
@@ -2,14 +2,17 @@ FROM rust:1.70-slim AS builder
 
 WORKDIR /app
 
-# Copy Cargo files
+# Copy only Cargo files
 COPY rust-compressor/Cargo.toml rust-compressor/Cargo.lock ./
 
 # Dummy build to cache dependencies
-RUN mkdir src && echo "pub fn dummy() {}" > src/lib.rs && cargo build --release --lib
+RUN mkdir src && \
+    echo "pub fn dummy() {}" > src/lib.rs && \
+    echo "[lib]\npath = \"src/lib.rs\"" >> Cargo.toml && \
+    cargo build --release --lib
 
-# Now copy the actual source code
+# Now copy actual sources
 COPY rust-compressor/src ./src
 
 # Build real library
-RUN cargo build --release --verbose --lib
+RUN cargo build --release --lib
